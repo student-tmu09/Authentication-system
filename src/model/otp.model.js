7558fd4b5d8c4536d57bef28aa3c2aff3485d466
Ayush kumar <thakuraayush741@gmail.com>
2026-09-20 15:36:35 +0530

    Add files via upload

--- src/model/otp.model.js
+++ src/model/otp.model.js
@@ -1,1 +1,23 @@
+const mongoose=require("mongoose")
 
+const otpSchema=new mongoose.Schema(
+    {
+        email:{
+            type:String,
+            required:true
+        },
+        user:{
+            type:mongoose.Schema.Types.ObjectId,
+            required:true
+        },
+        otphash:{
+            type:String,
+            required:true
+        }
+    },{
+        timestamps:true
+    }
+)
+const Otp=mongoose.model("Otp",otpSchema)
+
+module.exports={Otp}
