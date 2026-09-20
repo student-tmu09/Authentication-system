edcf33f6d270b888da3f891b6ec1f1083c710046
Ayush kumar <thakuraayush741@gmail.com>
2026-09-20 15:48:48 +0530

    Add files via upload

--- src/utils/Asynchandler.utils.js
+++ src/utils/Asynchandler.utils.js
@@ -1,1 +1,11 @@
-
+const AsyncHandler=(fnc)=>async(req,res,next)=>{
+    try{
+        return await fnc(req,res,next)
+    }catch(error){
+        res.status(error.code ||500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+module.exports = {AsyncHandler}
