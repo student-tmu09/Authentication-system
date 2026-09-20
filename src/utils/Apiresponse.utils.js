05e97491cf6bd4ed572020ffe2f170c6933c55fb
Ayush kumar <thakuraayush741@gmail.com>
2026-09-20 15:47:20 +0530

    Add files via upload

--- src/utils/Apiresponse.utils.js
+++ src/utils/Apiresponse.utils.js
@@ -1,1 +1,15 @@
-
+class Apiresponse{
+    constructor(
+        statuscode,
+        data,
+        message = "Response sent successfully"
+    )
+    {
+        this.statuscode=statuscode
+        this.data=data
+        this.message=message
+        this.success=statuscode<400
+      
+    }
+}
+module.exports={Apiresponse}
